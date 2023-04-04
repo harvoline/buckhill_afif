80e5b8c9d71ed31bde869f75012b049dfde154d3
modems <mohd.norafif@gmail.com>
2023-04-05 02:26:34 +0800

    Modify users table migration to follow UML

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->uuid();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->tinyInteger('is_admin', false)->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->char('avatar', 36)->nullable(); # placeholder for image UUID. wont be use in this task
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->tinyInteger('is_marketing', false)->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
