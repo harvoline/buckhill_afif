bb2590fb3a1f03e6f0fb09c0993f2d5db458f54c
modems <mohd.norafif@gmail.com>
2023-04-06 00:03:49 +0800

    Implement Jwt Token to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\JwtToken;
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Token\Builder;
+use Lcobucci\JWT\ValidationData;
+use Illuminate\Auth\Authenticatable;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Validation\Validator;
+use Illuminate\Database\Eloquent\Model;
+use Lcobucci\JWT\Encoding\ChainedFormatter;
+use Lcobucci\JWT\Validation\Constraint\IssuedBy;
+use Lcobucci\JWT\Validation\Constraint\IdentifiedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as AuthenticatableContract;
 
-class User extends Authenticatable
+class User extends AuthenticatableContract
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Authenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +28,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'uuid',
+        'is_admin',
         'email',
         'password',
     ];
@@ -41,4 +54,48 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function generateToken()
+    {
+        $datetime = new \DateTimeImmutable();
+        $signingKey   = InMemory::plainText('jwt-token');
+
+        $token = (new Builder(new JoseEncoder(), ChainedFormatter::default()))
+            ->issuedBy(env('APP_URL'))
+            ->permittedFor(env('APP_URL'))
+            ->identifiedBy(uniqid(), true)
+            ->issuedAt($datetime)
+            ->expiresAt($datetime->modify('+1 day')) // 24 hours
+            ->withClaim('user_id', $this->id)
+            ->withClaim('user_uuid', $this->uuid)
+            ->getToken(new Sha256(), $signingKey);
+
+        return $token->toString();
+    }
+
+    public function verifyToken($token)
+    {
+        $parsedToken = (new Parser(new JoseEncoder()))->parse($token);
+        $jwtToken = JwtToken::where('unique_id', $parsedToken->claims()->get('user_uuid'))->first();
+
+        if (!$jwtToken) {
+            return false;
+        }
+
+        if ($parsedToken->isExpired(now())) {
+            return false;
+        }
+
+        $validator = new Validator();
+
+        if (!$validator->validate($parsedToken, new IssuedBy(env('APP_URL')))) {
+            return false;
+        }
+
+        if (!$validator->validate($parsedToken, new IdentifiedBy($parsedToken->claims()->get('jti')))) {
+            return false;
+        }
+
+        return true;
+    }
 }
