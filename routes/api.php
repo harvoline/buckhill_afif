88b9df4d9f0b96e560ddba71086ddc23f63cab22
modems <mohd.norafif@gmail.com>
2023-04-06 00:07:42 +0800

    Add admin route API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::post('register', [AdminController::class, 'register']);
+        Route::post('login', [AdminController::class, 'login']);
+        Route::post('logout', [AdminController::class, 'logout']);
+        // Route::post('register', 'AdminController@register');
+        // Route::post('login', 'AdminController@login');
+        // Route::post('logout', 'AdminController@logout');
+
+        Route::middleware('jwt-admin')->group(function () {
+            Route::get('users', [AdminController::class, 'getUsers']);
+            Route::put('users/{user}', [AdminController::class, 'updateUser']);
+            Route::delete('users/{user}', [AdminController::class, 'deleteUser']);
+            // Route::get('users', 'AdminController@getUsers');
+            // Route::put('users/{user}', 'AdminController@updateUser');
+            // Route::delete('users/{user}', 'AdminController@deleteUser');
+        });
+    });
 });
