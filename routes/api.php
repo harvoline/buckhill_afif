43aca8ef6951347ff1bbf5a25b00d057db419c96
modems <mohd.norafif@gmail.com>
2023-04-07 23:04:31 +0800

    Update API route to use the new controller function and follow requirements

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\UserAuthController;
+use App\Http\Controllers\Api\AdminAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,20 +24,37 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::prefix('admin')->group(function () {
-        Route::post('register', [AdminController::class, 'register']);
-        Route::post('login', [AdminController::class, 'login']);
-        Route::post('logout', [AdminController::class, 'logout']);
-        // Route::post('register', 'AdminController@register');
-        // Route::post('login', 'AdminController@login');
-        // Route::post('logout', 'AdminController@logout');
+        // Route::post('register', [AdminAuthController::class, 'register']);
+        Route::post('login', [AdminAuthController::class, 'login']);
 
         Route::middleware('jwt-admin')->group(function () {
-            Route::get('users', [AdminController::class, 'getUsers']);
-            Route::put('users/{user}', [AdminController::class, 'updateUser']);
-            Route::delete('users/{user}', [AdminController::class, 'deleteUser']);
-            // Route::get('users', 'AdminController@getUsers');
-            // Route::put('users/{user}', 'AdminController@updateUser');
-            // Route::delete('users/{user}', 'AdminController@deleteUser');
+            Route::post('logout', [AdminAuthController::class, 'logout']);
+            Route::get('user-list', [AdminAuthController::class, 'getUsers']);
+        });
+    });
+
+    Route::prefix('user')->group(function () {
+        // Route::post('register', [UserAuthController::class, 'register']);
+        Route::post('login', [UserAuthController::class, 'login']);
+
+        Route::middleware('jwt')->group(function () {
+            Route::post('logout', [UserAuthController::class, 'logout']);
+
+        });
+    });
+
+    Route::prefix('order')->group(function () {
+        Route::middleware('jwt')->group(function () {
+            Route::post('/create', [OrderController::class, 'store']);
+            Route::get('/{uuid}', [OrderController::class, 'show']);
+            Route::put('/{uuid}', [OrderController::class, 'update']);
+            Route::delete('/{uuid}', [OrderController::class, 'destroy']);
+        });
+    });
+
+    Route::prefix('orders')->group(function () {
+        Route::middleware('jwt')->group(function () {
+            Route::get('/', [OrderController::class, 'index']);
         });
     });
 });
